c69b30ccd19df8ed895ef5a0b059f8d8374bd570
Masahiro Hiramori <mshr-h@users.noreply.github.com>
2021-06-13 17:09:43 +0900

    add todo memo

--- video_recorder.py
+++ video_recorder.py
@@ -44,6 +44,7 @@ class VideoRecorder(object):
         self.logger.propagate = False
 
         # TODO: split inference engine into dedicated class
+        # TODO: async run
         self.labels = load_labels("models/imagenet_labels.txt")
         self.interpreter = tflite.Interpreter(
                 model_path="models/mobilenet_v2_1.0_224_quant_edgetpu.tflite",
